09c618a345c71febda84dda0bce48093a24487d3
remedylove <remedylove@users.noreply.github.com>
2020-10-12 18:50:43 +0000

    deploy: e6660c1ae01e15e34f2a309a4bcc53f171e2c379

---
+++ precache-manifest.4b92725f11f8bcba010794ba4ce869cd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b64fa3bb589fe5e6e7be07b4ec14e81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a53174c47ffce4a0d941",
+    "url": "/static/css/2.6f44476b.chunk.css"
+  },
+  {
+    "revision": "e75fa24286c0ed026c20",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a53174c47ffce4a0d941",
+    "url": "/static/js/2.4c9d436e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.4c9d436e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75fa24286c0ed026c20",
+    "url": "/static/js/main.47da0fd7.chunk.js"
+  },
+  {
+    "revision": "9d82212920806f3bdf3e",
+    "url": "/static/js/runtime-main.843fc0d9.js"
+  },
+  {
+    "revision": "7179dab91b7958801bce8201cbdd405c",
+    "url": "/static/media/LeftColumnImage.7179dab9.svg"
+  },
+  {
+    "revision": "cfd78acbb60cb4ff78be2da9318ca8b2",
+    "url": "/static/media/SignUpSuccess.cfd78acb.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
