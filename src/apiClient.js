608dd790502e6d824343ec8c4e581e45c5007c7c
remedylove <lukaasz.pierzchala@gmail.com>
2020-10-12 21:25:45 +0200

    CI setup

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_API_URL || 'http://localhost:5000';
+const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 export default axios.create({ baseURL });
